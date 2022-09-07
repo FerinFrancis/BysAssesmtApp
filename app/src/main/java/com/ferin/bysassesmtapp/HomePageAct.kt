2a114ce5fbbb1db0277df8fff8b44b480f53ed5b
ferin <ferinfrancis1997@gmail.com>
2022-09-07 13:23:06 +0530

    nav_drawer commit

--- app/src/main/java/com/ferin/bysassesmtapp/HomePageAct.kt
+++ app/src/main/java/com/ferin/bysassesmtapp/HomePageAct.kt
@@ -2,10 +2,53 @@ package com.ferin.bysassesmtapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class HomePageAct : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayoutVar: DrawerLayout
+    lateinit var navViewVar: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        drawerLayoutVar = findViewById(R.id.drawerLayout)
+        navViewVar = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayoutVar,R.string.open,R.string.close)
+        drawerLayoutVar.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navViewVar.setNavigationItemSelectedListener{
+
+            when(it.itemId){
+                R.id.nav_home -> Toast.makeText(applicationContext,"Home Clicked",Toast.LENGTH_SHORT).show()
+                R.id.nav_notification -> Toast.makeText(applicationContext,"Notification Clicked",Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext,"Settings Clicked",Toast.LENGTH_SHORT).show()
+                R.id.nav_about -> Toast.makeText(applicationContext,"About Clicked",Toast.LENGTH_SHORT).show()
+                R.id.nav_share -> Toast.makeText(applicationContext,"Share Clicked",Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+        {
+            return true
+        }
+        else{
+            return super.onOptionsItemSelected(item)
+
+        }
     }
 }
